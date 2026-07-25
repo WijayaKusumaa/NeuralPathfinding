fa2c66b2a93b518eeca1480da3fa74c2831188ce
AkhdarhWijaya <120603181+AkhdarhWijaya@users.noreply.github.com>
2026-07-25 22:59:33 +0700

    clean: remove unused assets, fix favicon path, and update gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/NeuralPathfinding/' : '/',
   plugins: [
     tailwindcss(),
     react()
   ],
 })
-
